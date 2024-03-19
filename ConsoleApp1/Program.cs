a98ed814eb59c0190267bb07eb3852767dcf8516
MariaShulyk <mari.shulyk.05@gmail.com>
2024-03-19 02:20:55 +0100

    Commit 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,17 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Peace!");
 Console.WriteLine("Love!");
+
+int[] arr = new[] { 1, 2, 3};
+Console.WriteLine(GetAvg(arr));
+
+static double GetAvg(int[] arr)
+{
+    int result = 0;
+    foreach (var num in arr)
+    {
+        result += num;
+    }
+
+    return (double)result / arr.Length;
+}
