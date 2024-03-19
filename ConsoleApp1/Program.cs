b6c3d4ae255760f83ab775008f20dee3fff2ce40
MariaShulyk <mari.shulyk.05@gmail.com>
2024-03-19 02:28:35 +0100

    Commit 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Love!");
 
 int[] arr = new[] { 1, 2, 3};
 Console.WriteLine(GetAvg(arr));
+Console.WriteLine(FindMax(arr));
 
 static double GetAvg(int[] arr)
 {
@@ -14,4 +15,18 @@ static double GetAvg(int[] arr)
     }
 
     return (double)result / arr.Length;
+}
+
+static int FindMax(int[] arr)
+{
+    int max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
 }
